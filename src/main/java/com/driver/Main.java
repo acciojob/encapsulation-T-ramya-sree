7bb84ebab6b111703ebaef029937f61adb865024
Ramya Shree <kramyasree505@gmail.com>
2024-06-05 22:25:07 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+        RWOnly obj=new RWOnly();
+        obj.setName("ramya");
+        String retrieveName= obj.getName();
+        System.out.println(retrieveName);
+    }
 }
